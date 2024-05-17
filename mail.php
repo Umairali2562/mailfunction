189bd217f919d9f4a90d21eabbb0616dc662f4a6
Umair Ali <umairali2562@gmail.com>
2024-05-17 08:40:00 +0500

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+$to = "umairali2562@gmail.com";
+$subject = "I am running Now";
+$message = "The program is running";
+$headers = "From: sender@example.com\r\n";
+$headers .= "Reply-To: sender@example.com\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully.";
+} else {
+    echo "Failed to send email.";
+}
+?>
